996e256d70687885fd86f200d974b8909c0c145a
NguyenHien <ndh31707@gmail.com>
2024-09-11 16:40:11 +0700

    done main page

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,15 +3,19 @@ import Item from './Item';
 
 export default function Index() {
     return (
-        <div className="pt-[71px] px-6 pb-9 h-[958px]">
-            <h2 className="font-bold text-lg leading-none text-customColor-primary py-2">My Auras</h2>
-            <nav className="mt-6 h-[464px] overflow-y-scroll pe-3 scrollbar-custom">
-                <ul>
-                    {listSidebar.map((text, index) => (
-                        <Item title={text} key={index} />
-                    ))}
-                </ul>
-            </nav>
-        </div>
+        <>
+            <h2 className="pt-[71px] font-bold text-lg leading-none text-customColor-primary py-2">My Auras</h2>
+            <div className="h-full overflow-y-auto scrollbar-custom">
+                <div className="h-dvh">
+                    <nav className="mt-6 pe-3">
+                        <ul>
+                            {listSidebar.map((text, index) => (
+                                <Item title={text} key={index} />
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </>
     );
 }
